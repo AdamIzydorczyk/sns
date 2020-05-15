558b213cae6b3c0896d105874d3f9b594057da5d
Adam Izydorczyk <adam.izydorczyk@itti.com.pl>
2020-05-15 16:58:15 +0200

    add comment integration tests

--- sns-operation/domain/src/test/groovy/tk/aizydorczyk/sns/operation/domain/comment/create/CommentCreateControllerTest.groovy
+++ sns-operation/domain/src/test/groovy/tk/aizydorczyk/sns/operation/domain/comment/create/CommentCreateControllerTest.groovy
@@ -1,4 +1,4 @@
-package tk.aizydorczyk.sns.operation.domain.post.update
+package tk.aizydorczyk.sns.operation.domain.comment.create
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.junit.Test
@@ -17,9 +17,9 @@ import spock.lang.Specification
 import tk.aizydorczyk.sns.common.infrastructure.converter.ConvertersConfiguration
 import tk.aizydorczyk.sns.common.infrastructure.mapper.MapperConfiguration
 import tk.aizydorczyk.sns.common.infrastructure.utils.UtilsConfiguration
+import tk.aizydorczyk.sns.operation.domain.comment.CommentDto
 import tk.aizydorczyk.sns.operation.domain.config.H2JpaConfiguation
 import tk.aizydorczyk.sns.operation.domain.config.TestTimeConfiguration
-import tk.aizydorczyk.sns.operation.domain.post.PostDto
 import tk.aizydorczyk.sns.operation.infrastructure.rest.RestConfiguration
 
 import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.AFTER_TEST_METHOD
@@ -27,46 +27,48 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = [H2JpaConfiguation,
-        UpdatePostCommandConfiguration,
+        CreateCommentCommandConfiguration,
         MapperConfiguration,
-        UpdatePostController,
+        CreateCommentController,
         UtilsConfiguration,
         TestTimeConfiguration,
         ConvertersConfiguration,
         RestConfiguration])
 @AutoConfigureMockMvc
-class PostUpdateControllerTest extends Specification {
+class CommentCreateControllerTest extends Specification {
 
     @Autowired
     private MockMvc mockMvc
 
-    private static final String INSERT_POST = "INSERT INTO PUBLIC.POST(id, created_at, created_by, deleted, modified_at, modified_by, version, content) VALUES(1, '2012-12-12 12:12:12', '2D1EBC5B7D2741979CF0E84451C5BBB1', false, NULL, NULL, 0, 'TEST')"
+    private static final String INSERT_POST = "INSERT INTO PUBLIC.POST(id, created_at, created_by, deleted, modified_at, modified_by, version, content) VALUES(1, '2012-12-12 12:12:12', '2D1EBC5B7D2741979CF0E84451C5BBB1', false, NULL, NULL, 0, 'TEST_POST')"
+
     private static final String DELETE_POSTS = "DELETE FROM POST"
+    private static final String DELETE_COMMENTS = "DELETE FROM COMMENT"
 
     @Test
     @SqlGroup([
-            @Sql(statements = INSERT_POST),
-            @Sql(statements = DELETE_POSTS, executionPhase = AFTER_TEST_METHOD)
+            @Sql(statements = [INSERT_POST]),
+            @Sql(statements = [DELETE_COMMENTS, DELETE_POSTS], executionPhase = AFTER_TEST_METHOD)
     ])
-    def "should update post and return with OK status"() throws Exception {
+    def "should created comment and return with CREATED status"() throws Exception {
         given:
-        def requestBody = asJsonString(new PostDto("UPDATED"))
+        def requestBody = asJsonString(new CommentDto("TEST"))
         def testUserUuid = UUID.randomUUID().toString()
         def timeInSeconds = TestTimeConfiguration.TEST_TIME.getEpochSecond()
         when:
         def result = mockMvc.perform(MockMvcRequestBuilders
-                .put("/posts/1")
+                .post("/posts/1/comments")
                 .content(requestBody)
                 .header("userUuid", testUserUuid)
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON))
 
         then:
-        result.andExpect(status().isOk())
+        result.andExpect(status().isCreated())
         and:
-        result.andExpect(MockMvcResultMatchers.jsonPath('$.content').value("UPDATED"))
-        result.andExpect(MockMvcResultMatchers.jsonPath('$.modifiedBy').value(testUserUuid))
-        result.andExpect(MockMvcResultMatchers.jsonPath('$.modifiedAt').value(timeInSeconds))
+        result.andExpect(MockMvcResultMatchers.jsonPath('$.comment').value("TEST"))
+        result.andExpect(MockMvcResultMatchers.jsonPath('$.createdBy').value(testUserUuid))
+        result.andExpect(MockMvcResultMatchers.jsonPath('$.createdAt').value(timeInSeconds))
     }
 
     static String asJsonString(Object obj) {
@@ -76,5 +78,4 @@ class PostUpdateControllerTest extends Specification {
             throw new RuntimeException(e)
         }
     }
-
 }
--- sns-operation/domain/src/test/groovy/tk/aizydorczyk/sns/operation/domain/comment/update/CommentUpdateControllerTest.groovy
+++ sns-operation/domain/src/test/groovy/tk/aizydorczyk/sns/operation/domain/comment/update/CommentUpdateControllerTest.groovy
@@ -1,4 +1,4 @@
-package tk.aizydorczyk.sns.operation.domain.post.update
+package tk.aizydorczyk.sns.operation.domain.comment.update
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.junit.Test
@@ -17,9 +17,9 @@ import spock.lang.Specification
 import tk.aizydorczyk.sns.common.infrastructure.converter.ConvertersConfiguration
 import tk.aizydorczyk.sns.common.infrastructure.mapper.MapperConfiguration
 import tk.aizydorczyk.sns.common.infrastructure.utils.UtilsConfiguration
+import tk.aizydorczyk.sns.operation.domain.comment.CommentDto
 import tk.aizydorczyk.sns.operation.domain.config.H2JpaConfiguation
 import tk.aizydorczyk.sns.operation.domain.config.TestTimeConfiguration
-import tk.aizydorczyk.sns.operation.domain.post.PostDto
 import tk.aizydorczyk.sns.operation.infrastructure.rest.RestConfiguration
 
 import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.AFTER_TEST_METHOD
@@ -27,35 +27,38 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = [H2JpaConfiguation,
-        UpdatePostCommandConfiguration,
+        UpdateCommentCommandConfiguration,
         MapperConfiguration,
-        UpdatePostController,
+        UpdateCommentController,
         UtilsConfiguration,
         TestTimeConfiguration,
         ConvertersConfiguration,
         RestConfiguration])
 @AutoConfigureMockMvc
-class PostUpdateControllerTest extends Specification {
+class CommentUpdateControllerTest extends Specification {
 
     @Autowired
     private MockMvc mockMvc
 
-    private static final String INSERT_POST = "INSERT INTO PUBLIC.POST(id, created_at, created_by, deleted, modified_at, modified_by, version, content) VALUES(1, '2012-12-12 12:12:12', '2D1EBC5B7D2741979CF0E84451C5BBB1', false, NULL, NULL, 0, 'TEST')"
+    private static final String INSERT_POST = "INSERT INTO PUBLIC.POST(id, created_at, created_by, deleted, modified_at, modified_by, version, content) VALUES(1, '2012-12-12 12:12:12', '2D1EBC5B7D2741979CF0E84451C5BBB1', false, NULL, NULL, 0, 'TEST_POST')"
+    private static final String INSERT_COMMENT = "INSERT INTO PUBLIC.COMMENT(id, created_at, created_by, deleted, modified_at, modified_by, version, post_id, comment) VALUES(1, '2012-12-12 12:12:12', '2D1EBC5B7D2741979CF0E84451C5BBB1', false, NULL, NULL, 0, 1, 'TEST_COMMENT')"
+
     private static final String DELETE_POSTS = "DELETE FROM POST"
+    private static final String DELETE_COMMENTS = "DELETE FROM COMMENT"
 
     @Test
     @SqlGroup([
-            @Sql(statements = INSERT_POST),
-            @Sql(statements = DELETE_POSTS, executionPhase = AFTER_TEST_METHOD)
+            @Sql(statements = [INSERT_POST, INSERT_COMMENT]),
+            @Sql(statements = [DELETE_COMMENTS, DELETE_POSTS], executionPhase = AFTER_TEST_METHOD)
     ])
     def "should update post and return with OK status"() throws Exception {
         given:
-        def requestBody = asJsonString(new PostDto("UPDATED"))
+        def requestBody = asJsonString(new CommentDto("UPDATED"))
         def testUserUuid = UUID.randomUUID().toString()
         def timeInSeconds = TestTimeConfiguration.TEST_TIME.getEpochSecond()
         when:
         def result = mockMvc.perform(MockMvcRequestBuilders
-                .put("/posts/1")
+                .put("/posts/1/comments/1")
                 .content(requestBody)
                 .header("userUuid", testUserUuid)
                 .contentType(MediaType.APPLICATION_JSON)
@@ -64,7 +67,7 @@ class PostUpdateControllerTest extends Specification {
         then:
         result.andExpect(status().isOk())
         and:
-        result.andExpect(MockMvcResultMatchers.jsonPath('$.content').value("UPDATED"))
+        result.andExpect(MockMvcResultMatchers.jsonPath('$.comment').value("UPDATED"))
         result.andExpect(MockMvcResultMatchers.jsonPath('$.modifiedBy').value(testUserUuid))
         result.andExpect(MockMvcResultMatchers.jsonPath('$.modifiedAt').value(timeInSeconds))
     }
